293aeabf464e83f94b6a157b123c000e9bc7bc2a
Allen <allen.enrile@sjsu.edu>
2015-04-06 15:38:35 -0700

    testing code, prints out title and link

--- parser.java
+++ parser.java
@@ -2,24 +2,41 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
 public class parser {
 	public static void main (String[] args)
-	{
-		
-		String html = "<html><head><title>First parse</title></head>";
-		
+	{	
 		Document doc = null;
+		String hostname = "http://sciencenetlinks.com";
+		
+		// begin testing code
+		
 		try {
 			doc = Jsoup.connect("http://sciencenetlinks.com/search/?q=&content_types=Tool&s=1").get();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Elements newsHeadlines = doc.select(".detail");
-		System.out.println(newsHeadlines);
+		Elements titles = doc.select(".detail h1 a");
+		
+		// get titles/links and print out
+		for (Element e : titles)
+		{
+			String link = hostname + e.attr("href");
+			System.out.println(e.text().substring(0 ,e.text().length()-2));
+			System.out.println(link);
+		}
+
+		// end testing code
+		
+		return;
+	}
+	
+	private void strip ()
+	{
 		return;
 	}
 }
