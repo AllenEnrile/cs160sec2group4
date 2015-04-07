a55107260fa327246062880169b0d5b38329646c
Allen <allen.enrile@sjsu.edu>
2015-04-07 15:01:11 -0700

    abs url, sql changes

--- cs160/team4/Creation.java
+++ cs160/team4/Creation.java
@@ -1,22 +1,25 @@
 package cs160.team4;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Creation {
 
-	public static void main (String[] args)
+	public static void main (String[] args) throws FileNotFoundException, UnsupportedEncodingException
 	{
 		HashMap<String, HashMap<String, String>> tools = new HashMap<String, HashMap<String,String>>();
 		Parser prsr = new Parser("http://sciencenetlinks.com", tools);
 		ToSQL sql = new ToSQL(tools);
-//		sql.hashtoSQL(tools);
 		try {
 			prsr.parse();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		sql.hashtoSQL(tools);
 	}
 }
