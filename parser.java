f05d08da4cbe5b6204f76ee15803d8c73690d3ea
Chris Zelenka <Zelenka.chris@gmail.com>
2015-04-06 23:36:22 -0700

    pageSpecificParser coded

--- parser.java
+++ parser.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.sql.Timestamp;
+import java.util.Date;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -42,7 +45,7 @@ public class parser {
 				Document specific_Tool = Jsoup.connect(link).get();
 				
 				// begin page specific parsing
-				//pageSpecificParser(specific_Tool, tools.get(title));
+				pageSpecificParser(specific_Tool, tools.get(title));
 				j++; // go to next tool
 			}
 			System.out.println("Processing page " + String.valueOf(i));
@@ -78,15 +81,32 @@ public class parser {
 	 * @param title: hashmap for the specific tool
 	 */
 	private static void pageSpecificParser (Document doc, HashMap title)
-	{
+	{		
 		/* TO-DO:
-		 * 	lesson link
-		 * 	lesson image
-		 * 	category
-		 * 	author
-		 * 	content type
-		 * 	time stamp
+		 * 	lesson link		= doc.select(".content").get(0).getElementsByTag("a").first().attr("href")
+		 * 	lesson image	= .feature-image
+		 * 	category		= .block-list child(1)
+		 * 	author			= none
+		 * 	content type	= .block-list child(2)
+		 * 	time stamp		= new Timestamp(new java.util.Date().getTime())
 		 */
+		String link, image, timestamp;
+		ArrayList category = new ArrayList<String>(), content = new ArrayList<String>();
+		
+		link = doc.select(".content").get(0).getElementsByTag("a").first().attr("href");	//lesson link
+		image = doc.select(".feature-image").select("img").first().absUrl("src");	// lesson image		
+		Elements e = doc.select(".block-list").get(1).getElementsByTag("a");
+		for (Element s : e)		category.add(s.text());	// categories		
+		e = doc.select(".block-list").get(2).getElementsByTag("a");
+		for (Element s : e)		content.add(s.text());	// content types		
+		timestamp = new Timestamp(new java.util.Date().getTime()).toString();	// time stamp
+		
+		title.put("link", link);
+		title.put("image", image);
+		title.put("category", category);
+		title.put("content", content);
+		title.put("timestamp", timestamp);
+
 		return;
 	}
 }
