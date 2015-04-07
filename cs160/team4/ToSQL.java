29841467ee24da16ae84268e0b789cb12f9c5eda
Allen Enrile <aenrile1773@gmail.com>
2015-04-07 09:21:10 -0700

    write sql import message

--- cs160/team4/ToSQL.java
+++ cs160/team4/ToSQL.java
@@ -1,5 +1,8 @@
 package cs160.team4;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,8 +19,10 @@ public class ToSQL {
 	 * @param tools: hashmap of all tools
 	 * 
 	 * Will output to a text file containing the SQL import command
+	 * @throws UnsupportedEncodingException 
+	 * @throws FileNotFoundException 
 	 */
-	public void hashtoSQL (HashMap<String,HashMap<String,String>> tools)
+	public void hashtoSQL (HashMap<String,HashMap<String,String>> tools) throws FileNotFoundException, UnsupportedEncodingException
 	{
 		/*
 		 *  INSERT INTO `education` VALUES
@@ -25,25 +30,30 @@ public class ToSQL {
 		 *  
 		 *  
 		*/
-		
+		PrintWriter file = new PrintWriter("education_import.sql", "UTF-8");
 		// loop through the entire hashmap
+		file.println("INSERT INTO `education` VALUES");
 		for (Map.Entry<String, HashMap<String, String>> entry : tools.entrySet())
 		{
 			String title = entry.getKey(); // get the title
-			HashMap items = entry.getValue(); // get the title specifics
+			HashMap<String,String> items = entry.getValue(); // get the title specifics
+			StringBuilder statement = new StringBuilder(); // create insert statement
+			int index = 1;
 			
-			// obtain specifics
-			/*
-			items.get("desc");
-			items.get("link");
-			items.get("image");
-			items.get("category");
-			items.get("grades");
-			items.get("author");
-			items.get("content_type");
-			items.get("timestamp");
-			*/
+			// obtain specifics		
+			statement.append("('" + String.valueOf(index) + "',"); // ID
+			statement.append("'" + title + "',"); // TITLE
+			statement.append("'" + items.get("desc") + "',"); // DESCRIPTION
+			statement.append("'" + items.get("link") + "',"); // LINK
+			statement.append("'" + items.get("image") + "',"); // IMAGE
+			statement.append("'" + items.get("category") + "',"); // CAT
+			statement.append("'" + items.get("grades") + "',"); // GRADE LEVEL
+			statement.append("'',"); // AUTHOR
+			statement.append("'" + items.get("content_type") +"',"); // TYPE
+			statement.append("'" + items.get("timestamp") + "'),"); // TIME_SCRAPED			
+			index++;
+			file.println(statement); // write to file last comma needs to be semicolon
 		}
-		
+		file.close();
 	}
 }
